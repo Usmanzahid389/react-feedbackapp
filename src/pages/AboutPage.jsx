5041ae2c13a4c2808966562affb74c53e7c46998
usman <uz391110@gmail.com>
2022-03-22 18:51:42 +0500

    s

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,6 +8,8 @@ function AboutPage(props) {
         <h1>About This Project</h1>
         <p>This is a React app to leave feedback for a product or service</p>
         <p>Version: 1.0.0</p>
+        <p>github: usmanzahid389</p>
+
 
         <p>
           <Link to='/'>Back To Home</Link>
